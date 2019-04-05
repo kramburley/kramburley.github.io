040c5a761e306cc1b5c80bc00ecabac1b84deda7
kramburley <akirasendo7_13@yahoo.com>
2019-04-05 13:38:16 -0700

    added status page.. must fix design later

---
+++ js/index.js
@@ -0,0 +1,12 @@
+function display(window) {
+  disable_display();
+  document.getElementById(window).style.display = "block";
+}
+
+function disable_display() {
+  document.getElementById("status").style.display = "none";
+  document.getElementById("work").style.display = "none";
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("contact").style.display = "none";
+}
